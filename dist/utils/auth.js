a1e8db2676641c82d42b8d379ac7ed0d8bea26bf
Hector <hectorfiguereo26@gmail.com>
2023-03-08 17:40:25 -0400

    adding maxAge property to cookies

--- dist/utils/auth.js
+++ dist/utils/auth.js
@@ -3,6 +3,7 @@ export const setCookie = (key, payload) => {
         httpOnly: true,
         sameSite: "none",
         secure: true,
+        maxAge: 1 * 60 * 60 * 1000,
     };
     return [key, payload, cookieOptions];
 };
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -5,6 +5,7 @@ export const setCookie = (key: string, payload: unknown) => {
     httpOnly: true,
     sameSite: "none",
     secure: true,
+    maxAge: 1 * 60 * 60 * 1000,
   };
 
   return [key, payload, cookieOptions] as const;
